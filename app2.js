a53a08bc7674ac2cf66ab44058d558c9b2be8905
Alvin Adarsh <udayalvin@gmail.com>
2021-04-07 01:05:59 +0530

    footer

--- app2.js
+++ app2.js
@@ -2,8 +2,8 @@ const menuBtn = document.getElementsByClassName('menu-btn');
 let menuOpen = false;
 var menuSecondary = document.getElementsByClassName('nav-container-secondary');
 var body = document.getElementsByTagName('body');
-/* const section1 = document.getElementById('section1');
-const section2 = document.getElementById('section2');
+const section1 = document.getElementById('section1');
+/* const section2 = document.getElementById('section2');
 const section3 = document.getElementById('section3');
 const section4 = document.getElementById('section4');
 const section5 = document.getElementById('section5');
@@ -28,7 +28,25 @@ function clicked() {
     
 };
 
-function scrollto() {
+/* function scrollto1() {
+    document.documentElement.scrollIntoView(section2);
+  }
+function scrollto2() {
+    document.documentElement.scrollIntoView(section3);
+  }
+function scrollto3() {
+    document.documentElement.scrollIntoView(section4);
+  }
+function scrollto4() {
+    document.documentElement.scrollIntoView(section5);
+  }
+function scrollto5() {
+    document.documentElement.scrollIntoView(section6);
+  }
+function scrollto6() {
+    document.documentElement.scrollIntoView(section7);
+  } */
+function scrollto7() {
     document.documentElement.scrollIntoView(section1);
   }
   
